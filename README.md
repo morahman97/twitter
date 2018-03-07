33be433bf06f41c91758a801e32b774e4c442a43
Mohammed Rahman <m2rahman@ucsd.edu>
2018-03-06 23:36:51 -0800

    Required stories completed with minor bugs

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 **Twitter** is a basic twitter app to read and compose tweets the [Twitter API](https://apps.twitter.com/).
 
-Time spent: **X** hours spent in total
+Time spent: **3** hours spent in total
 
 ## User Stories
 
 The following **required** functionality is completed:
 
-- [ ] User sees app icon in home screen and styled launch screen. (1pt)
-- [ ] User can sign in using OAuth login flow. (1pt)
-- [ ] User can Logout. (1pt)
-- [ ] Create Data Models for User and Tweet. (1pt)
-- [ ] User can view last 20 tweets from their home timeline with the user profile picture, username, tweet text, and timestamp. (2pts)
-- [ ] User can pull to refresh. (1pt)
-- [ ] User can tap the retweet and favorite buttons in a tweet cell to retweet and/or favorite a tweet. (2pts)
-- [ ] Using AutoLayout, the Tweet cell should adjust it's layout for iPhone 7, Plus and SE device sizes as well as accommodate device rotation. (1pt)
+- [x] User sees app icon in home screen and styled launch screen. (1pt)
+- [x] User can sign in using OAuth login flow. (1pt)
+- [x] User can Logout. (1pt)
+- [x] Create Data Models for User and Tweet. (1pt)
+- [x] User can view last 20 tweets from their home timeline with the user profile picture, username, tweet text, and timestamp. (2pts)
+- [x] User can pull to refresh. (1pt)
+- [x] User can tap the retweet and favorite buttons in a tweet cell to retweet and/or favorite a tweet. (2pts)
+- [x] Using AutoLayout, the Tweet cell should adjust it's layout for iPhone 7, Plus and SE device sizes as well as accommodate device rotation. (1pt)
 
 The following **stretch** features are implemented:
 
@@ -31,20 +31,22 @@ The following **additional** features are implemented:
 
 Please list two areas of the assignment you'd like to **discuss further with your peers** during the next class (examples include better ways to implement something, how to extend your app in certain ways, etc):
 
-1.
-2.
+1. Difficulty understanding the Twitter API
+2. Issues with AutoLayout
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented user stories:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://i.imgur.com/oL2oZnK.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
 ## Notes
 
 Describe any challenges encountered while building the app.
+
+Challenges are listed above.
 
 ## Credits
 
--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -18,6 +18,8 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var createdAtLabel: UILabel!
     @IBOutlet weak var retweetCountLabel: UILabel!
     @IBOutlet weak var favoriteCountLabel: UILabel!
+    @IBOutlet var retweetButton: UIButton!
+    @IBOutlet var favoriteButton: UIButton!
     
     var tweet: Tweet! {
         didSet {
@@ -29,6 +31,65 @@ class TweetCell: UITableViewCell {
             createdAtLabel.text = tweet.createdAtString
             profileImage.af_setImage(withURL: tweet.user.profileImageURLHTTPS!)
         }
+    }
+    @IBAction func onRetweet(_ sender: Any) {
+        if (tweet.retweeted == false) {
+            tweet.retweeted = true
+            tweet.retweetCount += 1
+        }
+        else if (tweet.retweeted == true) {
+            tweet.retweeted = false
+            tweet.retweetCount -= 1
+        }
+        APIManager.shared.retweet(tweet) { (tweet: Tweet?, error: Error?) in
+            if let  error = error {
+                print("Error favoriting tweet: \(error.localizedDescription)")
+            } else if let tweet = tweet {
+                print("Successfully favorited the following Tweet: \n\(tweet.text)")
+            }
+        }
+        refreshData()
+    }
+    
+    func refreshData() {
+        if (tweet.favorited == true) {
+            self.favoriteButton.setImage(#imageLiteral(resourceName: "favor-icon-red"), for: UIControlState.normal)
+        }
+        else if (tweet.favorited == false) {
+            self.favoriteButton.setImage(#imageLiteral(resourceName: "favor-icon"), for: UIControlState.normal)
+        }
+        if (tweet.retweeted == true) {
+            self.retweetButton.setImage(#imageLiteral(resourceName: "retweet-icon-green"), for: UIControlState.normal)
+        }
+        else if (tweet.retweeted == false) {
+            self.retweetButton.setImage(#imageLiteral(resourceName: "retweet-icon"), for: UIControlState.normal)
+        }
+        tweetTextLabel.text = tweet.text
+        retweetCountLabel.text = String(describing: tweet.retweetCount)
+        favoriteCountLabel.text = String(describing: tweet.favoriteCount)
+        nameLabel.text = tweet.user.name
+        screen_nameLabel.text = tweet.user.screenName
+        createdAtLabel.text = tweet.createdAtString
+        profileImage.af_setImage(withURL: tweet.user.profileImageURLHTTPS!)
+    }
+    
+    @IBAction func onFavorite(_ sender: Any) {
+        if (tweet.favorited == false) {
+            tweet.favorited = true
+            tweet.favoriteCount += 1
+        }
+        else if (tweet.favorited == true) {
+            tweet.favorited = false
+            tweet.favoriteCount -= 1
+        }
+        APIManager.shared.favorite(tweet) { (tweet: Tweet?, error: Error?) in
+            if let  error = error {
+                print("Error favoriting tweet: \(error.localizedDescription)")
+            } else if let tweet = tweet {
+                print("Successfully favorited the following Tweet: \n\(tweet.text)")
+            }
+        }
+        refreshData()
     }
     
     override func awakeFromNib() {
